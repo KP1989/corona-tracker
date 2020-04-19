c14f9c4993275eefcac2d8daebc7c726f93106d4
BH14249 <kailash.kurmi@macys.com>
2020-04-19 18:54:42 +0100

    Updates

---
+++ precache-manifest.cbd2d9703244337435aef647fdcd9f99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fe854e9d4b074f58aafb5af0811bd1a",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "7fd0efc16ef72551145e",
+    "url": "/corona-tracker/static/css/main.09e66df1.chunk.css"
+  },
+  {
+    "revision": "cbb6066d4fe1a2e35080",
+    "url": "/corona-tracker/static/js/2.6f78a21b.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-tracker/static/js/2.6f78a21b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fd0efc16ef72551145e",
+    "url": "/corona-tracker/static/js/main.a1551795.chunk.js"
+  },
+  {
+    "revision": "3978a030d47e50bd1e16",
+    "url": "/corona-tracker/static/js/runtime-main.9a79d275.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona-tracker/static/media/image.d7265326.png"
+  }
+]);
